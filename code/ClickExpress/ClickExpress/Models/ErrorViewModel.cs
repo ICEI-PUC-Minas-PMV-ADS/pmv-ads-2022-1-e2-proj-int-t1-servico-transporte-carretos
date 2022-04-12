4facbf204fbbb31cb4b63ddde133ad6d44c325f0
WerlonOliveira <82004450+werlon-oliveira@users.noreply.github.com>
2022-04-11 22:34:15 -0300

    1a versao
    subindo primeira versão do código.

---
+++ code/ClickExpress/ClickExpress/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClickExpress.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
