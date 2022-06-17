8c79980e48e84e8f3b99d3781a76c9b0d1aeb7f4
Kenia <keniacunha100@gmail.com>
2022-06-17 16:18:04 -0300

    Exclusão das vies não utilizadas

--- code/ClickExpress/ClickExpress/Controllers/PedidosController.cs
+++ code/ClickExpress/ClickExpress/Controllers/PedidosController.cs
@@ -26,26 +26,7 @@ namespace ClickExpress.Controllers
             return View(await applicationDbContext.ToListAsync());
         }
 
-        // GET: Pedidos/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var pedido = await _context.Pedidos
-                .Include(p => p.Usuario)
-                .Include(p => p.Itens)
-                .Include(p => p.Orcamentos)
-                .FirstOrDefaultAsync(m => m.Id_contrato == id);
-            if (pedido == null)
-            {
-                return NotFound();
-            }
-
-            return View(pedido);
-        }
+ 
 
         // GET: Pedidos/Details/5
         public async Task<IActionResult> DetailsResposta(int? id)
@@ -120,90 +101,9 @@ namespace ClickExpress.Controllers
             }
             return Json(new { Erro = true });
         }
-
-        // GET: Pedidos/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var pedido = await _context.Pedidos.FindAsync(id);
-            if (pedido == null)
-            {
-                return NotFound();
-            }
-            ViewData["Id_usuario"] = new SelectList(_context.Usuarios, "Id_usuario", "Nome", pedido.Id_usuario);
-            return View(pedido);
-        }
-
-        // POST: Pedidos/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id_contrato,Dt_contrato,Tipo,End_Destino,End_Partida,Horario,Preco,Id_usuario")] Pedido pedido)
-        {
-            if (id != pedido.Id_contrato)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(pedido);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PedidoExists(pedido.Id_contrato))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["Id_usuario"] = new SelectList(_context.Usuarios, "Id_usuario", "Nome", pedido.Id_usuario);
-            return View(pedido);
-        }
-
-        // GET: Pedidos/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var pedido = await _context.Pedidos
-                .Include(p => p.Usuario)
-                .FirstOrDefaultAsync(m => m.Id_contrato == id);
-            if (pedido == null)
-            {
-                return NotFound();
-            }
-
-            return View(pedido);
-        }
-
-        // POST: Pedidos/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var pedido = await _context.Pedidos.FindAsync(id);
-            _context.Pedidos.Remove(pedido);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
+        
+        
+        
         private bool PedidoExists(int id)
         {
             return _context.Pedidos.Any(e => e.Id_contrato == id);
